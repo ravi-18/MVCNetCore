d1ca3e597662fd1aac874a6f44a04cdf8cb2a2c3
Dinda Ravi Algifari <dinda.ravi.algifari@gmail.com>
2020-12-02 14:32:54 +0700

    Generate webapp

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Articles()
+        {
+            var model = new ArticlesViewModel();
+            model.Articles = new ArticleRepository().GetLatest();
+
+            return View(model);
+        }
+    }
+}
